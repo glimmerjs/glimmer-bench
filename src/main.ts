e8f87c9c8b5e6294d24dcf09dada16f9bf8120fa
Kris Selden <kris.selden@gmail.com>
2017-12-12 09:58:26 -0800

    update glimmer to a version that works with the latest vm

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
-import Application from '@glimmer/application';
+import Application, { RuntimeCompilerLoader, DOMBuilder, SyncRenderer } from '@glimmer/application';
 import Resolver, { BasicModuleRegistry } from '@glimmer/resolver';
 import moduleMap from '../config/module-map';
 import resolverConfiguration from '../config/resolver-configuration';
+
 
 export default class App extends Application {
   constructor() {
     let moduleRegistry = new BasicModuleRegistry(moduleMap);
     let resolver = new Resolver(resolverConfiguration, moduleRegistry);
-
+    let loader = new RuntimeCompilerLoader(resolver);
+    let builder = new DOMBuilder({ element: document.body, nextSibling: null });
+    let renderer = new SyncRenderer();
     super({
+      builder,
+      renderer,
+      loader,
       resolver,
       rootName: resolverConfiguration.app.rootName
     });
